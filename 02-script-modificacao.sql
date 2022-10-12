4cb8cfd01eb538b7dd797ec281845facfe2231e8
Paulo Ferreira <phgferreira@outlook.com>
2022-10-11 23:02:49 -0300

    Inserido um tratamento default na hora de adicionar a columa descricao na tabela movimentacao

--- 02-script-modificacao.sql
+++ 02-script-modificacao.sql
@@ -1,11 +1,12 @@
 use financa_pessoal;
 
-alter table movimentacao add column descricao varchar(100) not null;
+alter table movimentacao add column descricao varchar(100) not null default 'Precisa informar uma descrição depois';
 /* ps: E se já tivermos dados inseridos, precisamos ter algum cuidado?
-** R: O MySQL preenche a coluna descricao com um valor default '' mas
-** para realmente preencher esses campos seria necessário pedir esse
-** histórico ao usuário
+** R: Para preencher a coluna descricao com um alerta pedindo para o usuário preencher essa informação depois,
+** usamos o default e para que o default não atrapalhe o not null que será utilizado para as próximas inserções
+** usamos o comando abaixo para só remover o default.
 */
+alter table movimentacao alter column descricao drop default;
 
 alter table movimentacao modify valor decimal(15,4);
 /* ps: Extra: E se fossemos diminuir a precisão, digamos de 6 casas
